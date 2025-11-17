66187234794013a9166022d7679ebcaf9d4dc0d7
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2025-11-17 20:48:08 +0100

    Update pj_game_manager.gd

--- test_folders/pj/scripts/pj_game_manager.gd
+++ test_folders/pj/scripts/pj_game_manager.gd
@@ -1,10 +1,19 @@
+class_name GameManager
 extends Node3D
+
+static var instance: GameManager
 
 @export var player: CharacterBody3D
 @export var shells = []
 
 func _ready() -> void:
-	EventBus.connect("game_restarted", restart_game)
+	if(instance == null):
+		instance = self;
+		EventBus.connect("game_restarted", restart_game);
+	else:
+		print("More than one GameManager exists!!!");
+		queue_free();
+
 
 func restart_game(level: int):
 	player.position = player.start_pos[level]

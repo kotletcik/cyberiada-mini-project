6165ad9d7ef9a77c4cf4e4ca0b4a12049a22d797
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2025-12-01 21:59:34 +0100

    test

--- test_folders/veeck/scripts/Mob.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody3D
-
-@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-		
-func _physics_process(delta: float) -> void:
-	move_and_slide()
--- test_folders/veeck/scripts/state_machine/States/Attack.gd
+++
@@ -1,9 +0,0 @@
-extends State
-class_name Attack
-	
-func Enter():
-	super.Enter()
-	attack()
-
-func attack():
-	EventBus.emit_signal("game_restarted")

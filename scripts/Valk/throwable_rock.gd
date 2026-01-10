fe119480b90a8c91b818fc0786abfb5d4ce4009b
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-01-10 17:13:46 +0100

    sound signal emission improvements

--- scripts/Valk/throwable_rock.gd
+++ scripts/Valk/throwable_rock.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 var player_pos: Vector3;
+@onready var signal_emmited: bool = false;
 
 func _ready() -> void:
     contact_monitor = true;
@@ -8,7 +9,9 @@ func _ready() -> void:
     continuous_cd = true;
 
 func _on_body_entered(body: Node) -> void:
+    if(signal_emmited): return;
     if(body.name == "Shell"):
         EventBus.sound_emitted_by_player.emit(player_pos);
     else:
         EventBus.sound_emitted_by_player.emit(global_position);
+    signal_emmited = true;
--- scripts/follow_sound.gd
+++ scripts/follow_sound.gd
@@ -11,7 +11,12 @@ func Enter():
 	follow_target.position = state_machine.target
 	nav_agent.target = follow_target
 	timer = follow_state_duration
-	
+	# Valk: dodałem aby potwór szedł do najnowszego dzwięku
+	EventBus.connect("sound_emitted_by_player", change_state_to_follow_sound)
+
+func change_state_to_follow_sound(sound_pos: Vector3):
+	state_machine.target = sound_pos
+	change_state_to("follow_sound")
 
 func Update(delta: float):
 	nav_agent.set_target_position(follow_target.position)

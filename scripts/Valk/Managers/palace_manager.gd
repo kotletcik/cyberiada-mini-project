f946d8ee5ee00c1ace3667553ed090150def5126
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-02-25 10:35:40 +0100

    Mind Palace start

--- scripts/Valk/Managers/palace_manager.gd
+++ scripts/Valk/Managers/palace_manager.gd
@@ -6,6 +6,8 @@ static var instance: PalaceManager;
 var gathered_clues: Array[Clue] = [null];
 var first_free_index: int = 0;
 
+@export var clue_paths: Array[CluePath] = [null];
+
 func _ready() -> void:
 	if(instance == null):
 		instance = self;
@@ -29,3 +31,13 @@ func remove_clue(clue_to_remove: Clue):
 				if(first_free_index > 0):
 					first_free_index -= 1;
 				return;
+
+
+func is_correct_thought(checked_clue: Clue, thought_clue: Clue) -> bool:
+	for i in range(0, clue_paths.size()):
+		for j in range(0, clue_paths[i].required_clues.size()):
+			if(clue_paths[i].required_clues[j] == checked_clue && clue_paths[i].is_clue_realized[j]):
+				var index: int = j + 1;
+				if(index >= clue_paths[i].required_clues.size() || clue_paths[i].is_clue_realized[j]): return false;
+				if(clue_paths[i].required_clues[index] == thought_clue): return true;
+	return false;

79a87e8fa0c2f34aa8280d4a59a05b85573cb8be
OpTiFall <62944006+OpTiFall@users.noreply.github.com>
2026-02-28 19:02:43 +0100

    AI fixes

--- scripts/Valk/shell_disappearance.gd
+++ scripts/Valk/shell_disappearance.gd
@@ -3,8 +3,9 @@ extends Node
 @export var shell_mesh: MeshInstance3D;
 
 func _ready() -> void:
-	EventBus.shells_appear.connect(set_shell_alpha.bind(1.0));
-	EventBus.shells_disappear.connect(set_shell_alpha.bind(0.0));
+	EventBus.shells_appear.connect(set_shell_alpha.bind(true));
+	EventBus.shells_disappear.connect(set_shell_alpha.bind(false));
 
-func set_shell_alpha(alpha: float) -> void:
-	shell_mesh.get_surface_override_material(0).albedo_color.a = alpha;
+func set_shell_alpha(visible: bool) -> void:
+	var parent: Node3D = get_parent();
+	parent.visible = visible;

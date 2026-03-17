437408d2f4428d06fb558611633bf51646d1f911
Valk0x29a <62944006+valk0x29a@users.noreply.github.com>
2026-03-17 14:13:02 +0100

    Eventy

--- scripts/Valk/player_interactions.gd
+++ scripts/Valk/player_interactions.gd
@@ -18,9 +18,13 @@ func _process(_delta: float) -> void:
 			# print(object.name);
 			if(object.is_in_group("Serum")):
 				InventoryManager.instance.add_item(ITEM_TYPE.SERUM, 1);
+				var pickup_item = object as PickupItem;
+				EventBus.call_event(pickup_item.event_on_pickup);
 				object.disable();
 			if(object.is_in_group("Rock")):
 				InventoryManager.instance.add_item(ITEM_TYPE.ROCK, 1);
+				var pickup_item = object as PickupItem;
+				EventBus.call_event(pickup_item.event_on_pickup);
 				object.disable();
 			if(object.has_method("player_interact")):
 				object.player_interact();
